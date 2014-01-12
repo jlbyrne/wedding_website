2eca0e0500131bedae756fc5d4178f6c727bd43f
Jennifer Byrne <jenniferbyrne@Jennifer-Byrnes-MacBook-Pro.local>
2014-01-11 21:01:51 -0800

    got all the view pages together

--- controllers/app_controller.rb
+++ controllers/app_controller.rb
@@ -6,29 +6,29 @@ get '/' do
 end
 
 get '/guest_list' do
-
+	erb :guest_list
 end
 
 get '/rsvp' do
-
+	erb :rsvp
 end
 
 get '/itinerary' do
-
+	erb :itinerary
 end
 
 get '/accomodations' do
-
+	erb :accomodations
 end
 
 get '/charleston' do
-
+	erb :charleston
 end
 
 get '/our_story' do
-
+	erb :our_story
 end
 
 get '/registry' do
-
+	erb :registry
 end
